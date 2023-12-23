b734e26b01d74af7a10b272f6752b23c326d4fb0
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2023-12-23 14:59:23 +0900

    create hexToColor func

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stocks_app/app/theme/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,7 +29,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.coloredTealAccent),
+        fontFamily: 'Inter',
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
